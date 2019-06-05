41f8d91b084fec4c4f68519b6b2e6f66f1f2012e
Neysi Alexandra Fermin Minaya <neysi_fermin@ucne.edu.do>
2019-06-05 10:09:31 -0400

    Personas BLL

--- RegistroPersonas/BLL/PersonasBLL.cs
+++ RegistroPersonas/BLL/PersonasBLL.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using RegistroPersonas.DAL;
+using RegistroPersonas.Entidades;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,184 +12,165 @@ namespace RegistroPersonas.BLL
 {
     public class PersonasBLL
     {
-        //En la clase de la BLL es donde va toda la programacion logica 
-
-        //Este es el metodo para guardar en la base de datos 
-        public static bool Guardar(Persona persona)
+        // METODO GUARDAR PERSONA
+        public static bool Guardar(Personas persona)
         {
             bool paso = false;
-            Contexto db = new Contexto();
+            Contexto contexto = new Contexto();
             try
             {
-                if (db.People.Add(persona) != null)
+                if (contexto.Persona.Add(persona) != null)
                 {
-                    db.SaveChanges();
+                    contexto.SaveChanges();
                     paso = true;
                 }
-
-                //siempre hay que cerrar la conexion, para esto es el dispose
-                db.Dispose();
+                contexto.Dispose();
             }
             catch (Exception)
             {
-
                 throw;
             }
             return paso;
         }
 
-        public static bool Guardar(TelefonoDetalle tel)
+        // METODO GUARDAR TELEFONO-DETALLE
+        public static bool Guardar(TelefonoDetalle telefono)
         {
             bool paso = false;
-            Contexto db = new Contexto();
+            Contexto contexto = new Contexto();
             try
             {
-                if (db.telefono.Add(tel) != null)
+                if (contexto.Telefono.Add(telefono) != null)
                 {
-                    db.SaveChanges();
+                    contexto.SaveChanges();
                     paso = true;
                 }
-
-                //siempre hay que cerrar la conexion, para esto es el dispose
-                db.Dispose();
+                contexto.Dispose();
             }
             catch (Exception)
             {
-
                 throw;
             }
             return paso;
         }
 
-        //Este es el metodo para Elminar en la base de datos 
+        // METODO ELIMINAR PERSONA
         public static bool Eliminar(int id)
         {
             bool paso = false;
 
-            Contexto db = new Contexto();
+            Contexto contexto = new Contexto();
             try
             {
-                var eliminar = db.People.Find(id);
-                db.Entry(eliminar).State = EntityState.Deleted;
-                if (db.SaveChanges() > 0)
+                var eliminar = contexto.Persona.Find(id);
+                contexto.Entry(eliminar).State = EntityState.Deleted;
+                if (contexto.SaveChanges() > 0)
                 {
                     paso = true;
                 }
-                db.Dispose();
+                contexto.Dispose();
             }
             catch (Exception)
             {
-
                 throw;
             }
             return paso;
         }
 
+        // METODO ELIMINAR TELEFONO-DETALLE
         public static bool EliminarDetalle(int id)
         {
             bool paso = false;
 
-            Contexto db = new Contexto();
+            Contexto contexto = new Contexto();
             try
             {
-                var eliminar = db.telefono.Find(id);
-                db.Entry(eliminar).State = EntityState.Deleted;
-                if (db.SaveChanges() > 0)
+                var eliminar = contexto.Telefono.Find(id);
+                contexto.Entry(eliminar).State = EntityState.Deleted;
+                if (contexto.SaveChanges() > 0)
                 {
                     paso = true;
                 }
-                db.Dispose();
+                contexto.Dispose();
             }
             catch (Exception)
             {
-
                 throw;
             }
             return paso;
         }
 
-        //Este es el metodo para Modificar en la base de datos 
-        public static bool Modificar(Persona persona)
+        // METODO MODIFICAR
+        public static bool Modificar(Personas persona)
         {
             bool paso = false;
-            Contexto db = new Contexto();
+            Contexto contexto = new Contexto();
             try
             {
-
-                db.Entry(persona).State = EntityState.Modified;
-                if (db.SaveChanges() > 0)
+                contexto.Entry(persona).State = EntityState.Modified;
+                if (contexto.SaveChanges() > 0)
                 {
                     paso = true;
-
                 }
 
                 foreach (var item in persona.Telefono)
                 {
-                    if (item.Id == 0)
+                    if (item.TelefonoDetalleId == 0)
                     {
                         Guardar(item);
                     }
                     else
                     {
-                        db.Entry(item).State = EntityState.Modified;
-                        if (db.SaveChanges() > 0)
+                        contexto.Entry(item).State = EntityState.Modified;
+                        if (contexto.SaveChanges() > 0)
                         {
                             paso = true;
 
                         }
                     }
-
-
                 }
-
-
-                db.Dispose();
+                contexto.Dispose();
             }
             catch (Exception)
             {
-
                 throw;
             }
             return paso;
         }
-
-        //Este es el metodo para buscar en la base de datos 
-        public static Persona Buscar(int id)
+        // METODO BUSCAR
+        public static Personas Buscar(int id)
         {
-            Contexto db = new Contexto();
-            Persona persona = new Persona();
+            Contexto contexto = new Contexto();
+            Personas persona = new Personas();
             try
             {
-                persona = db.People.Find(id);
+                persona = contexto.Persona.Find(id);
                 persona.Telefono.Count();
-                db.Dispose();
+                contexto.Dispose();
             }
             catch (Exception)
             {
-
                 throw;
             }
             return persona;
         }
 
-        //Este es el metodo para listar o consultar lo que teneos en la base de datos 
-        public static List<Persona> GetList(Expression<Func<Persona, bool>> persona)
+        // METODO LISTAR
+        public static List<Personas> GetList(Expression<Func<Personas, bool>> personas)
         {
-            List<Persona> people = new List<Persona>();
-            Contexto db = new Contexto();
+            List<Personas> persona = new List<Personas>();
+            Contexto contexto = new Contexto();
             try
             {
-                people = db.People.Where(persona).ToList();
-                people.Count();
-                db.Dispose();
+                persona = contexto.Persona.Where(personas).ToList();
+                persona.Count();
+                contexto.Dispose();
             }
             catch (Exception)
             {
-
                 throw;
             }
-
-            return people;
+            return persona;
         }
     }
 }
