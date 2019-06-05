96f708820c3907023b1efdb090bd604dec9b0232
Neysi Alexandra Fermin Minaya <neysi_fermin@ucne.edu.do>
2019-06-05 09:55:01 -0400

    Clase Telefono Detalle

--- RegistroPersonas/Entidades/TelefonoDetalle.cs
+++ RegistroPersonas/Entidades/TelefonoDetalle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,26 @@ namespace RegistroPersonas.Entidades
 {
     public class TelefonoDetalle
     {
+        [Key]
+        public int TelefonoDetalleId { get; set; }
+        public int PersonaId { get; set; }
+        public string TipoTelefono { get; set; }
+        public string Telefono { get; set; }
+
+        public TelefonoDetalle(int telefonoDetalleId, int personaId, string tipoTelefono, string telefono)
+        {
+            TelefonoDetalleId = telefonoDetalleId;
+            PersonaId = personaId;
+            TipoTelefono = tipoTelefono;
+            Telefono = telefono;
+        }
+
+        public TelefonoDetalle()
+        {
+            TelefonoDetalleId = 0;
+            PersonaId = 0;
+            TipoTelefono = string.Empty;
+            Telefono = string.Empty;
+        }
     }
 }
